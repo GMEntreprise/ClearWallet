261960b95c3926df35495a6ddc2fd30e74719612
GMWebDevelopment <gmwebdevelopments@gmail.com>
2024-10-23 16:04:25 +0200

    🎉 First commit begin :  adding compoenents

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row  items-center">
+        {/* Logo */}
+        <Image
+          src={"/clearwallet.png"}
+          alt="logo clear wallet"
+          width={40}
+          height={25}
+        />
+        <span className="text-blue-800 font-bold text-xl">ClearWallet</span>
+      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3  items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href="/sign-in">
+            <Button className="rounded-full">Get Started</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,36 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(241, 61%, 55%)",
+          foreground: "hsl(0, 0%, 100%)", // Texte blanc pour contraste
+        },
+        secondary: {
+          DEFAULT: "hsl(210, 50%, 30%)", // Un bleu légèrement plus foncé pour les accents
+          foreground: "hsl(0, 0%, 100%)",
+        },
+        muted: {
+          DEFAULT: "hsl(210, 50%, 60%)", // Un bleu plus clair pour les éléments secondaires
+          foreground: "hsl(0, 0%, 20%)", // Texte foncé
+        },
+        border: "hsl(210, 20%, 80%)", // Gris bleuâtre pour les bordures subtiles
+        input: "hsl(210, 20%, 95%)", // Bleu très clair pour les arrière-plans
+        ring: "hsl(210, 50%, 40%)", // Utiliser la couleur principale pour les anneaux
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
